ca0237f479a299415456b133697e755c44722dd9
orefayems <62698264+orefayems@users.noreply.github.com>
2022-10-15 03:07:36 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,23 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
+#from collections import Counter
+def frequencies(items): 
+	newList = []
 
-def frequencies(items):
-    frequencies = {}
-    # Your code goes here
-    return frequencies
+	for i in items:
+	
+		if i not in newList:
+			if type(i) is not str:
+				n = str(i)
+				index = items.index(i)
+				items[index] = n
+				newList.append(n)
+			else:
+				newList.append(i)
+			
+	frequencies = {}
+	for x in newList:
+		frequencies[x] = items.count(x)
+	return frequencies
+
+print(frequencies(items))
